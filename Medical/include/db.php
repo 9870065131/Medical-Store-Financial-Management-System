4564b133e749f99fb718c5bb0f9784cb7fe7b861
Shailesh Bhadani <33981829+9870065131@users.noreply.github.com>
2020-04-23 10:06:24 +0530

    Add files via upload

---
+++ Medical/include/db.php
@@ -0,0 +1,11 @@
+<?php 
+
+$databaseHost = 'localhost';
+$databaseUsername = 'root';
+$databasePassword = '';
+$databaseName = 'demo';
+
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+
+?>
+   
